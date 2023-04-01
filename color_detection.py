4806f28b2fd801423ed4983c3ffb347e29a771e4
mr-sombra <noelopezcamp2003@outlook.com>
2023-03-31 21:22:12 -0600

    rectangle detect Blu Yell Red design good

--- color_detection.py
+++ color_detection.py
@@ -10,15 +10,22 @@ MAX_RED_1 = np.array([10, 255, 255], np.uint8)
 MIN_RED_2 = np.array([170, 100, 20], np.uint8)
 MAX_RED_2 = np.array([180, 255, 255], np.uint8)
 
-MIN_YELLOW = np.array([28, 100, 20], np.uint8)
-MAX_YELLOW = np.array([32, 255, 255], np.uint8)
+MIN_YELLOW = np.array([15, 100, 20], np.uint8)
+MAX_YELLOW = np.array([45, 255, 255], np.uint8)
 
 
 def blue_detection(frame):
     frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     mask_blue = cv.inRange(frame_hsv, MIN_BLUE, MAX_BLUE)
-    mask_blue_visual = cv.bitwise_and(frame, frame, mask=mask_blue)
-    frame = mask_blue_visual
+    contours, _ = cv.findContours(mask_blue,
+                                  cv.RETR_EXTERNAL,
+                                  cv.CHAIN_APPROX_SIMPLE)
+    for i in contours:
+        area = cv.contourArea(i)
+        if area > 3000:
+            x, y, w, h = cv.boundingRect(i)
+            cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
+    # mask_blue_visual = cv.bitwise_and(frame, frame, mask=mask_blue)
     return frame
 
 
@@ -26,16 +33,28 @@ def red_detection(frame):
     frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     mask_red_1 = cv.inRange(frame_hsv, MIN_RED_1, MAX_RED_1)
     mask_red_2 = cv.inRange(frame_hsv, MIN_RED_2, MAX_RED_2)
-    mask_red = cv.add(mask_red_1, mask_red_2)
-    mask_red_visual = cv.bitwise_and(frame, frame, mask=mask_red)
-    frame = mask_red_visual
+    mask_red = cv.bitwise_or(mask_red_1, mask_red_2)
+    contours, _ = cv.findContours(mask_red,
+                                  cv.RETR_EXTERNAL,
+                                  cv.CHAIN_APPROX_SIMPLE)
+    for i in contours:
+        area = cv.contourArea(i)
+        if area > 3000:
+            x, y, w, h = cv.boundingRect(i)
+            cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
+    # mask_red_visual = cv.bitwise_and(frame, frame, mask=mask_red)
     return frame
 
 
 def yellow_detection(frame):
     frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     mask_yellow = cv.inRange(frame_hsv, MIN_YELLOW, MAX_YELLOW)
-    contours, hierarchy = cv.findContours(
-        mask_yellow, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(frame, contours, -1, (255, 0, 0), 3)
+    contours, _ = cv.findContours(mask_yellow,
+                                  cv.RETR_EXTERNAL,
+                                  cv.CHAIN_APPROX_SIMPLE)
+    for i in contours:
+        area = cv.contourArea(i)
+        if area > 3000:
+            x, y, w, h = cv.boundingRect(i)
+            cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
     return frame
